5ce0507dc5e5de0ada51f2e3449281ee9a94ec88
Pyae Mon Thant <pyamonthant.pmt@gmail.com>
2026-01-11 22:23:14 +0630

    Add initial GoGOTRavel data

--- backend/backend_project/backend_project/settings.py
+++ backend/backend_project/backend_project/settings.py
@@ -114,9 +114,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- backend/backend_project/users/models.py
+++ backend/backend_project/users/models.py
@@ -1,8 +1,22 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.username
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    
+    # Fix related_name conflicts
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # rename reverse relation
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',  # rename reverse relation
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
